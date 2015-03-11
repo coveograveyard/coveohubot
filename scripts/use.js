e9ab2f9e6d77fa26e0739e6713060c5d0cf420a8
Pierre-Alexandre St-Jean <pa@stjean.me>
2015-03-11 14:06:43 -0400

    Who knows...

--- scripts/use.js
+++ scripts/use.js
@@ -14,14 +14,15 @@ var _effectivenessReduce = function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a
 module.exports = function(robot){
   robot.hear(/^!use (.*)/,function(msg){
     var what = msg.match[1];
-    var splitted = what.split(" ")
+
+    var splitted = what.split(" ");
     if(splitted.length < 2){
       msg.send("Must specify an attack and a target\n>!use attack target");
       return;
     }
 
-    var target = splitted[1]
-    var atk = splitted[0]
+    var target = splitted.slice(1,-1).join(" ");
+    var atk = splitted[0];
 
     var textToSend = msg.message.user.name + " used " + atk + " on " + target + "! ";
 
